6ed2a2816c51534e16c756562be8bdb2014df69b
Andrii Bon <andreybon1@gmail.com>
2024-10-01 10:14:19 +0300

    Add project files.

---
+++ algo-practice-5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Input first number:";
+    cin >> a;
+    cout << "Input second number:";
+    cin >> b;
+    if (a > b)
+        cout << "";
+    else if (a < b)
+        cout << "";
+    else
+        cout << "";
+    return 0;
+}
